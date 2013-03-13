5e120c931f891537dba8fe5a0497fcda240edf99
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-13 15:26:48 +0100

    Release 0.1.1 (by lein-release)

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject com.ingemark/pbxis-ws "0.1.1-SNAPSHOT"
+(defproject com.ingemark/pbxis-ws "0.1.1"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :clojars}
   :aliases {"bundle-helper" "bundle-pbxis-ws"}
-  :plugins [[com.ingemark/lein-bundle "0.1.0-SNAPSHOT"]
-            [com.ingemark/lein-bundle-pbxis-ws "0.1.0-SNAPSHOT"]]
+  :plugins [[com.ingemark/lein-bundle "0.1.0"]
+            [com.ingemark/lein-bundle-pbxis-ws "0.1.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/data.json "0.2.1"]
-                 [com.ingemark/pbxis "0.5.1-alpha10"]
+                 [com.ingemark/pbxis "0.5.1"]
                  [net.cgrand/moustache "1.2.0-alpha2"]
                  [hiccup "1.0.2"]
                  [ring/ring-core "1.1.0" :exclusions [javax.servlet/servlet-api]]
