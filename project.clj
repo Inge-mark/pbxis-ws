09cfabd74c172b65022fc537276dea9f8236aad5
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-19 11:35:29 +0100

    Specify bundle repo

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :none}
-  :repositories [["releases" "s3p://pbxis-ws/releases"]]
+  :deploy-repositories [["bundle" "s3p://www.inge-mark.hr/pbxis-ws"]]
   :aliases {"bundle-helper" "bundle-pbxis-ws"}
   :plugins [[com.ingemark/lein-bundle-pbxis-ws "0.1.3-SNAPSHOT"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
