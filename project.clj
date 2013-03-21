eaa3a6c3ce80ad3b19ee11e94b22b6279a82e5db
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-21 17:29:59 +0100

    Uploading to SourceForge

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :lein-release {:deploy-via :none}
-  :deploy-repositories [["bundle" "s3p://www.inge-mark.hr/pbxis-ws"]]
+  :deploy-repositories [["bundle" "forge://mtopolnik@pbxis-ws"]]
   :aliases {"bundle" "bundle-pbxis-ws"
-            "publish-checkout" ["thrush" "bundle," "upload" "bundle" "bundle"]
+            "publish-checkout" ["thrush" "bundle," "upload" "bundle"]
             "publish-latest" ["with-checkout" ":latest" "publish-checkout"]}
   :plugins [[lein-thrush "0.1.1"]
             [com.ingemark/lein-release "2.0.9"]
             [com.ingemark/lein-with-checkout "0.1.0"]
             [com.ingemark/lein-bundle-pbxis-ws "0.1.4"]
-            [com.ingemark/lein-upload "0.1.1"]]
+            [com.ingemark/lein-upload "0.1.2"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/data.json "0.2.1"]
