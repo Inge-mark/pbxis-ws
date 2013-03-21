88dcac35da5ee8c50d3644a6793db9c637c19c2b
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-03-21 15:15:51 +0100

    Move to 0.1.13-SNAPSHOT (by lein-release)

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis-ws "0.1.12"
+(defproject com.ingemark/pbxis-ws "0.1.13-SNAPSHOT"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
