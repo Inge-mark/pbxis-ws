41929dcf1acda7c87f6d7627ebd906e804fa96b5
Marko Topolnik <mtopolnik@inge-mark.hr>
2013-04-01 23:06:04 +0200

    Upgrade pbxis to fix type error in queue actions

--- project.clj
+++ project.clj
@@ -16,7 +16,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/data.json "0.2.1"]
-                 [com.ingemark/pbxis "0.5.3"]
+                 [com.ingemark/pbxis "0.5.5"]
                  [net.cgrand/moustache "1.2.0-alpha2"]
                  [hiccup "1.0.2"]
                  [ring/ring-core "1.1.0" :exclusions [javax.servlet/servlet-api]]
