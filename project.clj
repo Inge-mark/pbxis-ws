36b9c1906c6b637adebc9c022e624e29c5f0de66
Marko Topolnik <marko.topolnik@inge-mark.com>
2015-02-24 16:29:19 +0100

    New snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis-ws "1.0.0"
+(defproject com.ingemark/pbxis-ws "1.0.1-SNAPSHOT"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
