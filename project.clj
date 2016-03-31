8e6735e7b0cc423c5efa778a2e29c2476ed24ea1
Viktor Matic <viktor@inge-mark.com>
2016-03-31 12:38:56 +0200

    New release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis-ws "1.0.1-SNAPSHOT"
+(defproject com.ingemark/pbxis-ws "1.0.1"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
