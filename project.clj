599a33a99f4c2098738393f436d861f930d0afa7
Josip Gracin <josip.gracin@ingemark.com>
2017-11-24 13:16:20 +0100

    upgraded to pbxis 2.0.2 (solving parking bug) and clojure 1.7.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis-ws "2.0.1"
+(defproject com.ingemark/pbxis-ws "2.0.2"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
@@ -18,14 +18,14 @@
             "publish-latest" ["with-checkout" ":latest"
                               "thrush" "uberjar," "bundle" ".," "upload" "bundle"]}
   :plugins [[lein-nix "0.1.9"]]
-  :dependencies [[com.ingemark/pbxis "2.0.1"]
-                 [org.clojure/clojure "1.5.1"]
+  :dependencies [[com.ingemark/pbxis "2.0.2"]
+                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.incubator "0.1.2"]
                  [org.clojure/data.json "0.2.1"]
                  [net.cgrand/moustache "1.2.0-alpha2"]
                  [hiccup "1.0.2"]
                  [ring/ring-core "1.1.0" :exclusions [javax.servlet/servlet-api]]
-                 [aleph "0.3.0-beta15"]
+                 [aleph "0.3.0"]
                  [org.slf4j/slf4j-api "1.7.2"]
                  [ch.qos.logback/logback-classic "1.0.9"]]
   :jvm-opts ["-Dlogback.configurationFile=logback.xml"]
