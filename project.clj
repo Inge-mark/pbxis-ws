3bb0251d86aef9fa89f560cf6df1060398587a92
Josip Gracin <josip.gracin@ingemark.com>
2018-05-17 11:27:50 +0200

    new snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ingemark/pbxis-ws "2.0.3"
+(defproject com.ingemark/pbxis-ws "2.0.4-SNAPSHOT"
   :description "Asterisk Call Center Web Service"
   :url "http://www.inge-mark.hr"
   :license {:name "Eclipse Public License"
